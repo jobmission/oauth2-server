960da71cca38d5afc70a280f65689f83c4392c83
jobmission <contact@revengemission.com>
2019-08-28 16:53:41 +0800

    fix refresh_token

--- src/main/java/com/revengemission/sso/oauth2/server/config/CustomTokenEnhancer.java
+++ src/main/java/com/revengemission/sso/oauth2/server/config/CustomTokenEnhancer.java
@@ -1,8 +1,13 @@
 package com.revengemission.sso.oauth2.server.config;
 
 import com.revengemission.sso.oauth2.server.domain.UserInfo;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
+import org.springframework.security.oauth2.common.util.JsonParser;
+import org.springframework.security.oauth2.common.util.JsonParserFactory;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 
@@ -11,27 +16,55 @@ import java.util.Map;
 
 public class CustomTokenEnhancer implements TokenEnhancer {
 
+    private JsonParser objectMapper = JsonParserFactory.create();
+
     /**
      * 自定义一些token属性
      *
-     * @param accessToken accessToken
+     * @param accessToken    accessToken
      * @param authentication authentication
      * @return OAuth2AccessToken
      */
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
         final Map<String, Object> additionalInformation = new HashMap<>(16);
-        // Important !,client_credentials mode ,no user!
-        if (authentication.getUserAuthentication() != null) {
-            // 与登录时候放进去的UserDetail实现类一致
-            UserInfo user = (UserInfo) authentication.getUserAuthentication().getPrincipal();
-            additionalInformation.put("grantType", authentication.getOAuth2Request().getGrantType());
-            additionalInformation.put("accountOpenCode", user.getAccountOpenCode());
-            additionalInformation.put("sub", user.getUsername());
+        if (authentication.getOAuth2Request().isRefresh()) {
+            Map<String, Object> map = decode(accessToken.getRefreshToken().getValue());
+            if (map.containsKey("accountOpenCode")) {
+                additionalInformation.put("accountOpenCode", map.get("accountOpenCode"));
+            }
+            if (map.containsKey("grantType")) {
+                additionalInformation.put("grantType", map.get("grantType"));
+            }
+            if (map.containsKey("sub")) {
+                additionalInformation.put("grantType", map.get("grantType"));
+            }
+            additionalInformation.put("sub", map.get("sub"));
             additionalInformation.put("status", 1);
+        } else {
+            // Important !,client_credentials mode ,no user!
+            if (authentication.getUserAuthentication() != null) {
+                // 与登录时候放进去的UserDetail实现类一致
+                UserInfo user = (UserInfo) authentication.getUserAuthentication().getPrincipal();
+                additionalInformation.put("grantType", authentication.getOAuth2Request().getGrantType());
+                additionalInformation.put("accountOpenCode", user.getAccountOpenCode());
+                additionalInformation.put("sub", user.getUsername());
+                additionalInformation.put("status", 1);
+            }
         }
+
         ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInformation);
         return accessToken;
     }
 
+    protected Map<String, Object> decode(String token) {
+        try {
+            Jwt jwt = JwtHelper.decode(token);
+            String claimsStr = jwt.getClaims();
+            Map<String, Object> claims = objectMapper.parseMap(claimsStr);
+            return claims;
+        } catch (Exception e) {
+            throw new InvalidTokenException("Cannot convert access token to JSON", e);
+        }
+    }
 }
